27ee1d01e2885e3e7e2ddd6a0451f99204e62891
ipekka2027 <ipekka2027@student.cis.edu.hk>
2024-08-31 08:33:40 +0800

    Update Main.java
    Included academic honesty pledge.

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int youngestAge = (7 + (age / 2));
+        String result = "The youngest person you can date is " + youngestAge + " years old.";
+        System.out.println(result);
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int pages = 100 - age;
+        String result = "The number of pages you should read before giving up on a book is " + pages;
+        System.out.println(result);
 
     }
 }
